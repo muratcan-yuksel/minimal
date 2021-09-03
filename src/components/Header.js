c9435e1eec983a50a3478d306c5affdf54502879
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-09-04 02:44:52 +0300

    tweaked the time in the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,16 +9,68 @@ function Header() {
           fontFamily: "Red Hat Display",
           color: "#3F3D56",
           fontWeight: 500,
-          fontSize: "5em",
+          fontSize: "3em",
         }}
         startDelay={2000}
         cursorColor="#3F3D56"
         multiText={[
-          "Hello world, this is Murat",
-          "I'm a front-end web developer",
-          "My main focus is creating React.js applications",
-          "Contact me by =>",
+          "Hello, world.",
+          // "I'm a front-end web developer",
+          // "My main focus is creating React.js applications",
+          // "Contact me by =>",
         ]}
+        multiTextDelay={1000}
+        typeSpeed={50}
+      />
+      <TypeWriterEffect
+        textStyle={{
+          fontFamily: "Red Hat Display",
+          color: "#3F3D56",
+          fontWeight: 500,
+          fontSize: "3em",
+        }}
+        startDelay={3500}
+        cursorColor="#3F3D56"
+        multiText={["This is Murat."]}
+        multiTextDelay={1000}
+        typeSpeed={50}
+      />
+      <TypeWriterEffect
+        textStyle={{
+          fontFamily: "Red Hat Display",
+          color: "#3F3D56",
+          fontWeight: 500,
+          fontSize: "3em",
+        }}
+        startDelay={5500}
+        cursorColor="#3F3D56"
+        multiText={["I'm a front-end web developer."]}
+        multiTextDelay={1000}
+        typeSpeed={50}
+      />
+      <TypeWriterEffect
+        textStyle={{
+          fontFamily: "Red Hat Display",
+          color: "#3F3D56",
+          fontWeight: 500,
+          fontSize: "3em",
+        }}
+        startDelay={8000}
+        cursorColor="#3F3D56"
+        multiText={["My main focus is creating React.js applications."]}
+        multiTextDelay={1000}
+        typeSpeed={50}
+      />
+      <TypeWriterEffect
+        textStyle={{
+          fontFamily: "Red Hat Display",
+          color: "#3F3D56",
+          fontWeight: 500,
+          fontSize: "3em",
+        }}
+        startDelay={11000}
+        cursorColor="#3F3D56"
+        multiText={["Contact me by =>"]}
         multiTextDelay={1000}
         typeSpeed={50}
       />
